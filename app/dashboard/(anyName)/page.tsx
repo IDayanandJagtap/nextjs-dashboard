f1cc6b8106871bd6006135c95c80f21473028c65
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2023-11-18 10:57:28 +0530

    Chapter 9

---
+++ app/dashboard/(anyName)/page.tsx
@@ -0,0 +1,38 @@
+import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+import {
+    CardsSkeleton,
+    LatestInvoicesSkeleton,
+    RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                {/* Next js continue chapter LINE (gradient) effect */}
+                {/* <div
+                    aria-hidden="true"
+                    className="mx-auto h-32 w-[2px] bg-gradient-to-t from-blue-300 md:h-48"
+                ></div> */}
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
+    );
+}
---
+++ app/dashboard/(anyName)/readme.md
@@ -0,0 +1,6 @@
+// This folder doesn't affects the route
+
+-   folder created using ()
+-   This was done to keep loading.tsx limited to the home(dashboard) page only
+-   If it was one directory up then the invoices and customers routes would also have rendered the same loading.tsx
+-   But we have used suspense so now there is no use of loading.tsx ... still we have kept the page.tsx here to demonstrate the use of loading page and (anyName) folder
